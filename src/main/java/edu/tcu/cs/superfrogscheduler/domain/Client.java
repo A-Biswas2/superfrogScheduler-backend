945b5129ec262db54aa792132d3c5b49d828edae
Mehmet Firat Aksoy <m.firat.aksoy@tcu.edu>
2022-04-29 19:37:16 -0500

    Update Client.java
    client attributes fixed

--- src/main/java/edu/tcu/cs/superfrogscheduler/domain/Client.java
+++ src/main/java/edu/tcu/cs/superfrogscheduler/domain/Client.java
@@ -13,10 +13,46 @@ public class Client implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    private String username;
     @JsonIgnore
     private String password;
     private boolean enabled;
+    private String firstname;
+    private String lastname;
+    private String phoneNumber;
+    private String email;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     private String roles; // comma separated strings
 
     public Client() {
@@ -28,14 +64,6 @@ public class Client implements Serializable {
 
     public void setId(Integer id) {
         this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     public String getPassword() {
